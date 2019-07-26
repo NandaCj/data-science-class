9124b7133f6a73bf69902c5fa66768caf183493f
mageswarand <mageswaran.dhandapani@imaginea.com>
2019-07-26 09:14:06 +0530

    lambda

---
+++ Python/lamda.py
@@ -0,0 +1,32 @@
+import functools
+
+fun = lambda x, y : x*y
+
+# print(fun(10, 20))
+
+
+
+def func(x, y):
+    return x*y
+
+# print(func(10, 20))
+
+
+#--------------------- filter ----------------
+L = [1,2,3,4,5,6]
+
+Y = filter(lambda x : x%2 ==0 , L)
+
+print(Y)
+print(list(Y))
+
+#--------------------- Map ----------------
+
+M = map(lambda x: x*x , L)
+print(list(M))
+
+#--------------------- reduce ----------------
+
+R = functools.reduce(lambda x, y : x+y , L)
+
+
